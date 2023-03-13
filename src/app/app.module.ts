c634169cfa95530c401d3fc815948af98eb84d51
Thiago Ribeiro <thiagobcoelho25@gmail.com>
2023-03-13 18:53:08 -0300

    create sections-theme

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TickerNewsComponent } from './components/ticker-news/ticker-news.component';
 import { CarouselSliderComponent } from './components/carousel-slider/carousel-slider.component';
+import { SectionsThemeComponent } from './components/sections-theme/sections-theme.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TickerNewsComponent,
-    CarouselSliderComponent
+    CarouselSliderComponent,
+    SectionsThemeComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/sections-theme/sections-theme.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sections-theme',
+  templateUrl: './sections-theme.component.html',
+  styleUrls: ['./sections-theme.component.css']
+})
+export class SectionsThemeComponent {
+
+}
